49c41772d722b38ba71d2d36730e8bb1b29beffa
JN <jeannoelseneque@hotmail.com>
2020-07-01 12:14:20 +0930

    Checking in the starter project from where we left off

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetBool("walk", true);
+            anim.SetBool("punch", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            anim.SetBool("walk", false);
+            anim.SetBool("punch", true);
+        }
+    }
+}
---
+++ Assets/Scripts/CameraRotator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CameraRotator : MonoBehaviour
+{
+    public float speed;
+    public bool rotate = true;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.S))
+        //{
+        //    rotate = !rotate;
+
+        //}
+
+        //if (rotate)
+        //{
+            transform.Rotate(0, speed * Time.deltaTime, 0);
+        //}
+        
+    }
+}
